01082dda077133ca78b9a643df462b41ab50367a
osvaldobnu <osvaldobnu@gmail.com>
2025-09-23 18:00:56 -0300

    Update next.config.ts
    Ajuste para ignorar erros de build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // mantém o React em modo estrito
+  eslint: {
+    // Ignora erros de ESLint durante o build (ex: 'any', 'prefer-const')
+    ignoreDuringBuilds: true,
+  },
+  // Aqui você pode adicionar outras configurações, como imagens externas, rewrites, etc.
 };
 
 export default nextConfig;
